4d23dee94c32736c961e580fe8aab65cd71c7b30
GRACE <kwaham.grace@gmail.com>
2024-10-15 14:41:15 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ if [ "${MYSQL_ROOT_PASSWORD}" = "" ] ;then MYSQL_ROOT_PASSWORD=123456 ;fi
 
 service mariadb start
 
-mysqlgrace -u root password ${MYSQL_ROOT_PASSWORD}
+mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
 
 exec /server --createDatabase=true
